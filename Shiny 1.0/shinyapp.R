84d45c39b84d4af69c845ab2bb719ac14ece4b54
CharlineChamp <92438465+CharlineChamp@users.noreply.github.com>
2021-11-09 08:06:13 +0100

    Update shinyapp.R

--- Shiny 1.0/shinyapp.R
+++ Shiny 1.0/shinyapp.R
@@ -29,12 +29,22 @@ ui <- dashboardPage(
                                   fluidRow(column(width = 6, offset = 3, align = "center",includeMarkdown("www/Accueil.Rmd"))),
                                   fluidRow(column(width = 6,offset= 2,tags$img(src='logoblanc.png', height = '120', width ='198')))
       ),
-      tabItem(tabName = "Pilotes",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE,status = "info", style="circle",selectInput("anneepilotes", label = NULL,choices = c("Sélectionnez une année",seq(1950,2020, by =1))),selected ="choisir")))
+      tabItem(tabName = "Pilotes",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE,status = "info", style="circle",selectInput("anneepilotes", label = NULL,choices = seq(2020,1950, by =-1)),selected ="choisir"))),
+              fluidRow(column(width = 12, box(title = "Position finale des pilotes ayant fait le tour le plus rapide",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("posfinal")))),
+              fluidRow(column(width = 12, box(title = "Représentation des points gagnés par pilote par grand prix",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("ptdrivergp")))),
+              fluidRow(column(width = 12, box(title = "Nombre de points moyens par grand prix de chaque pilote",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("ptmoy")))),
+              fluidRow(column(width = 12, box(title = "Grand Prix", width = NULL, solidHeader = TRUE,status = "info", style="circle",uiOutput("grandprix"),selected ="choisir"))),
+              fluidRow(column(width = 12, box(title = "Comparaison des positions de départ et d'arrivée des pilotes",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL, plotlyOutput("posdeparr"))))
       ),
-      tabItem(tabName = "Ecuries",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE, status = "info",style="circle",selectInput("anneeecuries", label = NULL,choices = c("Sélectionnez une année",seq(1950,2020, by =1))),selected ="choisir"))),
-                                  fluidRow(column(width = 12,box(plotOutput("plot1"))))
+      tabItem(tabName = "Ecuries",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE, status = "info",style="circle",selectInput("anneeecuries", label = NULL,choices = seq(2020,1950, by =-1)),selected ="choisir"))),
+              fluidRow(column(width = 12, box(title = "Pourcentage de victoires par écurrie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyse")))),
+              fluidRow(column(width = 12, box(title = "Représentation de la somme des points gagnés par écurie par grand prix",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL, plotlyOutput("pointecuriegp"))))
       ),
-      tabItem(tabName = "Abandon"),
+      tabItem(tabName = "Abandon",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE, status = "info",style="circle",selectInput("anneeabandon", label = NULL,choices = seq(2020,1950, by =-1)),selected ="choisir"))),
+              fluidRow(column(width = 12, box(title = "Nombre de DNF et DNS de chaque écurie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("dnfteam")))),
+              fluidRow(column(width = 12, box(title = "Nombre de DNF et DNS de chaque pilote",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL, plotlyOutput("dnfd")))),
+              fluidRow(column(width = 12, box(title = "Nombre de DNF et DNS de chaque grand prix",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("dnfgp"))))
+      ),
       tabItem(tabName = "Introduction"),
       tabItem(tabName = "Conclusion"),
       tabItem(tabName = "Questionnaire")
@@ -44,7 +54,7 @@ ui <- dashboardPage(
   dashboardthemes::shinyDashboardThemes(theme = "grey_dark")
   
   ),
-  footer = dashboardFooter(left= "En cours de création...", skin ="black"),
+  footer = dashboardFooter(left= "En cours de création..."),
   #Titre dans le navigateur 
   title = "Projet F1"
   #as.numeric(format(Sys.Date(),"%Y")
