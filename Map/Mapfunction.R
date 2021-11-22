023f0d5b09da1dd34126d51c8800cfd8de4e134f
CharlineChamp <92438465+CharlineChamp@users.noreply.github.com>
2021-11-22 16:20:38 +0100

    update

--- Map/Mapfunction.R
+++ Map/Mapfunction.R
@@ -7,7 +7,7 @@ MapGP <- function(data){
     cbind((data$Lng), (data$Lat)),
     data.frame(type = factor(1:length(data$Lng))))
   
-  Icon <- makeIcon(iconUrl = "www/test.png",iconWidth= 70,iconHeight= 35)
+  Icon <- makeIcon(iconUrl = "www/test.png",iconWidth= 20,iconHeight= 20)
   #Création de la carte interactive
   leaflet(df) %>% addTiles()%>% addMarkers(icon = Icon) %>%addProviderTiles("CartoDB.DarkMatter")
   
