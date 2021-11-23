13fddf41f48dba0032ec57022b8c453e83ce923b
CharlineChamp <92438465+CharlineChamp@users.noreply.github.com>
2021-11-23 17:28:24 +0100

    update shiny

--- Shiny/3.5/shinyapp.R
+++ Shiny/3.5/shinyapp.R
@@ -9,16 +9,16 @@ library(VroumVroum)
 library(leaflet)
 
 #Textes utiles pour interprétation graphique
-g1 <- "Sur votre gauche vous remarquerez un diagramme en barre. Ce diagramme représente la fréquence de la position d'arrivée du pilote  ayant fait le tour le plus rapide. On s'attend à ce que la personne qui a fait le tour le plus rapide soit dans les premières positions, or ce n'est pas toujours le cas. Les pilotes ayant fait le tour le plus rapide finissent relativement dans le TOP 10. Il se peut que lors d'un grand prix un pilote ayant eu le meilleur tour ne finisse pas la course. Ainsi, cela sera matérialiser par un position égale à 0."   
+g1 <- "Sur votre gauche vous remarquerez un diagramme en barre. Ce diagramme représente la fréquence de la position d'arrivée du pilote  ayant fait le tour le plus rapide. On s'attend à ce que la personne qui a fait le tour le plus rapide soit dans les premières positions, or ce n'est pas toujours le cas. Les pilotes ayant fait le tour le plus rapide finissent relativement dans le TOP 10. Il se peut que lors d'un grand prix un pilote ayant eu le meilleur tour ne finisse pas la course. Ainsi, cela sera matérialisé par un position égale à 0."   
 g2 <- "Ici nous avons une représentation des points gagnés par les pilotes pour chaque grand prix sur une saison. Pour vous aidez à réaliser des comparaisons vous pouvez double cliquer sur un pilote pour obtenir sa courbe. Vous pouvez ensuite sélectionner plusieurs pilotes en cliquant sur leur nom. Si vous voulez revenir sur le graphique de départ veuillez double cliquer deux fois sur un nom de pilote."
-g3 <- "Ce diagramme en barre représente le nombre moyen de points par pilote sur une saison. Remarquons qu'à l'aide de ce graphique nous pouvons retrouvons un semblant du classement final des pilotes. La barre la plus haute représentant le leader."
+g3 <- "Ce diagramme en barre représente le nombre moyen de points par pilote sur une saison. Remarquons qu'à l'aide de ce graphique nous pouvons retrouver un semblant du classement final des pilotes. La barre la plus haute représentant le leader."
 g4 <- "Sur votre droite nous avons tracé un nuage de point. Il représente la position d'arrivée en fonction de la position de départ sur un grand prix. Ce dernier que vous pouvez choisir à l'aide du menu déroulant. Si un point se trouve sur la droite cela signifie que le pilote est arrivé à la même position dont il est parti. En revanche, s'il se trouve en dessous de celle-ci, le pilote aura gagné des places et l'inverse s'il se trouve au dessus. Pour mieux comprendre, vous pouvez placer votre curseur sur l'un des points. Cela vous indiquera le nom du pilote, sa position de départ et celle d'arrivée. Si un pilote n'est pas présent sur ce graphique c'est qu'il n'a pas fini la course."
 g5 <- "Ici vous trouverez un diagramme circulaire représentant le pourcentage de victoires d'une écurie sur une saison. Nous pouvons remarquer une diversité sur les écuries remportant des grands prix. On peut également remarquer au fil des années l'évolution des écuries."
-g6 <- "A votre droite, vous trouverez un graphique réprésentant la somme des points gagnés par les pilotes des écuries par grand prix. Pour vous aider à réaliser des comparaisons vous pouvez double cliquer sur une écurie pour obtenir sa courbe. Vous pouvez ensuite sélectionnez plusieurs écuries en cliquant sur leur noms. Si vous voulez revenir sur le graphique de départ, veuillez double cliquer deux fois sur un nom d'écurie."
-g7 <- "Ce diagramme circulaire représente le pourcentage d'abandon pour chaque écuries lors d'une saison. Nous considérons comme abandon tous ceux n'ayant pas pris les départs mais également ceux qui n'ont pas terminé la course."
-g8 <- "Sur votre droite, nous avons un diagramme en barres représentant le nombre d'abandons par pilotes sur une saison. Remarquons que le nombre d'abandons n'est pas forcément lié au classement final. Les pilotes n'aparaissant pas sur le graphique sont ceux ayant débuté et fini tous les grands prix."
+g6 <- "A votre droite, vous trouverez un graphique réprésentant la somme des points gagnés par les pilotes des écuries par grand prix. Pour vous aidez à réaliser des comparaisons vous pouvez double cliquer sur une écurie pour obtenir sa courbe. Vous pouvez ensuite sélectionnez plusieurs écuries en cliquant sur leur noms. Si vous voulez revenir sur le graphique de départ, veuillez double cliquer deux fois sur un nom d'écurie."
+g7 <- "Ce diagramme circulaire représente le pourcentage d'abandon pour chaque écurie lors d'une saison. Nous considérons comme abandon tous ceux n'ayant pas pris les départs mais également ceux qui n'ont pas terminé la course."
+g8 <- "Sur votre droite, nous avons un diagramme en barre représentant le nombre d'abandons par pilote sur une saison. Remarquons que le nombre d'abandons n'est pas forcément lié au classement final. Les pilotes n'aparaissant pas sur le graphique sont ceux ayant débutés et finis tous les grands prix."
 g9 <- "Ici vous trouverez un diagramme en barre représentant le nombre d'abandons par grand prix sur une saison. On pourrait se demander si les circuits ayant eu le plus d'abandons sont les plus dangereux. D'autres facteurs peuvent rentrer en jeu comme la météo."
-g10 <- "Sur votre gauche vous remarquerez un diagramme en barre. Ce diagramme représente la fréquence d'abandon de chaque pilotes entre deux années choisies. Vous remarquerez que plus l'écart entre les deux années est grand plus la graphe est dense. Ceci s'explique du fait de l'ajout de pilotes. De plus, plus la barre sera haute plus le pilote a de chance de ne pas finir sa course."
+g10 <- "Sur votre gauche vous remarquerez un diagramme en barre. Ce diagramme représente la fréquence d'abandons de chaque pilote entre deux années choisies. Vous remarquerez que plus l'écart entre les deux années est grand plus le graphe est dense. Ceci s'explique du fait de l'ajout de pilotes. De plus, plus la barre sera haute plus le pilote a de chance de ne pas finir sa course."
 
 ui <- dashboardPage(
   #Tableau du haut
@@ -26,7 +26,7 @@ ui <- dashboardPage(
                            dropdownMenu(type = "messages", 
                                         icon = icon("info-circle"),
                                         badgeStatus =NULL,
-                                        headerText = ("Cette Application est en cours de création. Dernière mise à jour réalisée le 22 nov. 2021"))
+                                        headerText = ("Cette Application est en cours de création. Dernière mise à jour réalisée le 23 nov. 2021"))
   ),
   #Tableau de gauche
   sidebar = dashboardSidebar(
@@ -44,7 +44,7 @@ ui <- dashboardPage(
                menuSubItem("formula1.com", icon = icon("arrow-alt-circle-right"),href = "https://www.formula1.com/en/results.html/2021/races.html"),
                menuSubItem("racing-statistics.com",icon = icon("arrow-alt-circle-right"), href = "https://www.racing-statistics.com/en/seasons")
       ),
-      menuItem("Aide", tabName = "Aide",icon = icon("question-circle"))
+      menuItem("Pour les curieux", tabName = "Curieux",icon = icon("question-circle"))
     )
   ),
   body = dashboardBody(
@@ -72,7 +72,7 @@ ui <- dashboardPage(
                        column(width = 8, box(title = "Comparaison des positions de départ et d'arrivée des pilotes",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL, plotlyOutput("posdeparr"))))
       ),
       tabItem(tabName = "Ecuries",fluidRow(column(width = 12, box(title = "Année", width = NULL, solidHeader = TRUE, status = "info",style="circle",selectInput("anneeecuries", label = NULL,choices = seq(2020,1950, by =-1)),selected ="choisir"))),
-              fluidRow(column(width = 8, box(title = "Pourcentage de victoires par écurrie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyse"))),
+              fluidRow(column(width = 8, box(title = "Pourcentage de victoires par écurie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyse"))),
                        column(width = 4, box(title = "Interprétation",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,g5))
               ),
               fluidRow(column(width = 3, box(title = "Interprétation",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,g6)),
@@ -97,14 +97,14 @@ ui <- dashboardPage(
                                                column(width = 6, box(title = "Année n°2", width = NULL, solidHeader = TRUE,status = "info", style="circle",selectInput("anneecompar2", label = NULL,choices = seq(2020,1950, by =-1)),selected ="choisir"))),
               fluidRow(column(width = 6, box(title = "Nombre de points moyens pour chaque pilote",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("ptmoyA1"))),
                        column(width = 6, box(title = "Nombre de points moyens pour chaque pilote",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("ptmoyA2")))),
-              fluidRow(column(width = 6, box(title = "Pourcentage de victoires par écurrie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyseA1"))),
-                       column(width = 6, box(title = "Pourcentage de victoires par écurrie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyseA2")))),
-              fluidRow(column(width = 9, box(title = "Fréqeunce d'abandons de chaque pilote entre deux années choisies",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("dnffrq"))),
+              fluidRow(column(width = 6, box(title = "Pourcentage de victoires par écurie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyseA1"))),
+                       column(width = 6, box(title = "Pourcentage de victoires par écurie",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("datateamanalyseA2")))),
+              fluidRow(column(width = 9, box(title = "Fréquence d'abandons de chaque pilote entre deux années choisies",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,plotlyOutput("dnffrq"))),
                        column(width = 3, box(title = "Interprétation",width = NULL,solidHeader = TRUE, status = "info",style="circle",label = NULL,g10))
               )
               
       ),
-      tabItem(tabName = "Aide", fluidRow(column(width =12, tags$iframe(style="width: 100%", height= 800 ,src= "Help.html"))))
+      tabItem(tabName = "Curieux", fluidRow(column(width =12, tags$iframe(style="width: 100%", height= 800 ,src= "Help.html"))))
     )
   ),
   #Titre dans le navigateur
